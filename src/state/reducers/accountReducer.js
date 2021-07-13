e8befe75e70a0ac7c381a35c8e39130ea62c02ae
nyer <nyeritsian@gmail.com>
2021-07-13 16:34:49 +0400

    feat: add state folder,use redux

---
+++ src/state/reducers/accountReducer.js
@@ -0,0 +1,12 @@
+const reducer = (initialState = 0, action) => {
+    switch (action.type) {
+        case "deposit":
+            return initialState + action.payload
+        case "withdraw":
+            return initialState - action.payload
+        default:
+            return initialState
+    }
+};
+
+export default reducer;
